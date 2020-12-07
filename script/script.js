cbf2a7baa5a39630095073b41062dc53dac13103
DeveloperGregory <69530744+DeveloperGregory@users.noreply.github.com>
2020-12-07 15:39:35 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const contactTextLinks ={
+    "profile-link" : '<a href="https://www.freecodecamp.org/fcc1efbc055-4ab5-45c4-add9-907d1865cf6e" target="_blank">My Free Code Camp Profile</a>',
+    "twitter-link" : '<a href="https://twitter.com/developer_greg" target="_blank">My Twitter Account</a>',
+    "git-link" : '<a href="https://github.com/DeveloperGregory" target="_blank">My Git Hub</a>',
+    "email-link" : '<a href="">My Email Address</a>'
+};
+const contactIconLinks ={
+    "profile-link" : '<a href="https://www.freecodecamp.org/fcc1efbc055-4ab5-45c4-add9-907d1865cf6e" target="_blank"><i class="fab fa-free-code-camp"></i></a>',
+    "twitter-link" : '<a href="https://twitter.com/developer_greg" target="_blank"><i class="fab fa-twitter"></i></a>',
+    "git-link" : '<a href="https://github.com/DeveloperGregory" target="_blank"><i class="fab fa-github"></i></a>',
+    "email-link" : '<a href=""><i class="fas fa-at"></i></a>'
+};
+
+$(document).ready(function (){
+    $('#my-name').addClass('animate__animated animate__lightSpeedInLeft');
+    $('#my-title').addClass('animate__animated animate__lightSpeedInRight');
+    
+});
